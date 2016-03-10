e60b25e02dc1c4501892eb370e85b19414647932
Adel Ahmadyan <ahmadyan@gmail.com>
2016-03-10 13:26:47 -0600

    random tree algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
 	Graph* g = new Graph();
-	int playerSize = 100;
-	int graphConnectivityFactor = 2;
+	int playerSize = 20;
+	int graphConnectivityFactor = 3 ;
 	int resourceSize = 10;
 	//g->generateRandomGraph(playerSize, graphConnectivityFactor);
-	g->generateErdosRenyiRandomGraph(playerSize, 0.05);
+	g->generateErdosRenyiRandomGraph(playerSize, 0.4);
     Search* search = new Search(playerSize, resourceSize, g);
     //search->run();
     search->run();
