e7d0e13ab1a77f3d9d1bc6d56ccbcf39708f577c
Adel Ahmadyan <ahmadyan@gmail.com>
2016-03-10 11:20:34 -0600

    added paragraph about random tree

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char * argv[]) {
 	Graph* g = new Graph();
 	int playerSize = 100;
 	int graphConnectivityFactor = 2;
-	int resourceSize = 5;
+	int resourceSize = 10;
 	//g->generateRandomGraph(playerSize, graphConnectivityFactor);
 	g->generateErdosRenyiRandomGraph(playerSize, 0.05);
     Search* search = new Search(playerSize, resourceSize, g);
